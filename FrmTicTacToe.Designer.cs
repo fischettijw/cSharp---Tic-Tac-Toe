4bd3f993dfb182e15811df5c3ad56a1d90a94727
Joseph Fischetti <fischettijw@gmail.com>
2020-02-27 08:55:02 -0500

    Add project files.

---
+++ FrmTicTacToe.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace cSharp___Tic_Tac_Toe
+{
+    partial class FrmTicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xPnlTicTacToe = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // xPnlTicTacToe
+            // 
+            this.xPnlTicTacToe.Location = new System.Drawing.Point(26, 75);
+            this.xPnlTicTacToe.Name = "xPnlTicTacToe";
+            this.xPnlTicTacToe.Size = new System.Drawing.Size(400, 400);
+            this.xPnlTicTacToe.TabIndex = 0;
+            // 
+            // FrmTicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 565);
+            this.Controls.Add(this.xPnlTicTacToe);
+            this.Name = "FrmTicTacToe";
+            this.Load += new System.EventHandler(this.FrmTicTacToe_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel xPnlTicTacToe;
+    }
+}
+
---
+++ FrmTicTacToe.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharp___Tic_Tac_Toe
+{
+
+    public partial class FrmTicTacToe : Form
+    {
+        static Button[,] btnRC = new Button[3, 3];
+        public FrmTicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        public void FrmTicTacToe_Load(object sender, EventArgs e)
+        {
+            CustomizeForm();
+            CreateTicTacToeBoard();
+
+        }
+
+        private void CustomizeForm()
+        {
+            this.ClientSize = new Size(new Point(440, 600));
+            this.Text = "Tic-Tac-Toe   by Joseph Fischetti";
+
+            Label LblTicTacToeTitle = new Label();
+            this.Controls.Add(LblTicTacToeTitle);
+            LblTicTacToeTitle.AutoSize = false;
+            LblTicTacToeTitle.Size = new Size(this.ClientSize.Width, 60);
+            LblTicTacToeTitle.Location = new Point(0, 20);
+            LblTicTacToeTitle.Text = "Tic-Tac-Toe";
+            LblTicTacToeTitle.Font = new Font("Nightclub BTN", 36);
+            LblTicTacToeTitle.TextAlign = ContentAlignment.MiddleCenter;
+
+            Panel PnlTicTacToe = new Panel();
+            this.Controls.Add(PnlTicTacToe);
+            
+            this.PnlTicTacToe.Size = new Size(100, 100);
+
+
+
+        }
+
+        public void CreateTicTacToeBoard()
+        {
+            int btnSize = xPnlTicTacToe.Width / 4;
+            int btnSpacing = (xPnlTicTacToe.Width - (btnSize * 3)) / 4;
+
+            for (int r = 0; r <= 2; r++)
+            {
+                for (int c = 0; c <= 2; c++)
+                {
+                    btnRC[r, c] = new Button();
+                    xPnlTicTacToe.Controls.Add(btnRC[r, c]);
+
+                    btnRC[r, c].Text = $"{(r * 3) + c}"; //"X";
+                    btnRC[r, c].TextAlign = ContentAlignment.MiddleCenter;
+                    btnRC[r, c].Font = new Font("Nightclub BTN", 48);
+                    int x = (btnSpacing * (c + 1)) + (c * btnSize);
+                    int y = (btnSpacing * (r + 1)) + (r * btnSize);
+                    btnRC[r, c].Location = new Point(x, y);
+                    btnRC[r, c].Size = new Size(new Point(btnSize, btnSize));
+
+                    btnRC[r, c].Click += FrmTicTacToe_Click;
+                }
+            }
+        }
+
+        private void FrmTicTacToe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((sender as Button).Text);
+        }
+    }
+}
+
+
+//https://youtu.be/6CM5x4B6BKA?t=561
