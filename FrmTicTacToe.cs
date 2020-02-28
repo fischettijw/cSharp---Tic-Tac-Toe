3963b2f5cd1e2e167cd64dc5740e79cc0d0fb9cb
Joseph Fischetti <fischettijw@gmail.com>
2020-02-28 07:05:24 -0500

    Updated Form Code

--- FrmTicTacToe.cs
+++ FrmTicTacToe.cs
@@ -100,5 +100,10 @@ namespace cSharp___Tic_Tac_Toe
     }
 }
 
-// Simple AI
+//      Simple AI for Computer Player
+//priority 1:  can computer player get tick tac toe
+//priority 2:  block opponent tic tac toe
+//priority 3:  go for corner space
+//priority 4:  pick open space
+
 //https://youtu.be/6CM5x4B6BKA?t=561
